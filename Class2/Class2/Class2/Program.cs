dbfeaca29cd65c42234ddfeebc6261a26c14f556
Nen0-pro <nenadpopovski@yahoo.com>
2020-03-05 19:25:27 +0100

    intital commit

---
+++ Class2/Class2/Class2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Class2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstDouble = 10.10;
+            double secondDouble = 20.10;
+
+            double firstResult = firstDouble / secondDouble;
+            Console.WriteLine(firstResult);
+
+            int firstNumber = 30;
+            int secondNumber = 100;
+
+            int secondResult = secondNumber / firstNumber ;
+            Console.WriteLine(secondResult);
+
+            Console.ReadLine();
+        }
+    }
+}
